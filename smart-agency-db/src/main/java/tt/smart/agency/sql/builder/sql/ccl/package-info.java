37bd0c190c8e6758e4e08261218bdcdeb22be7ac
ooail <1184010001@qq.com>
2025-04-23 15:00:24 +0800

    FIX：说明。

---
+++ smart-agency-db/src/main/java/tt/smart/agency/sql/builder/sql/ccl/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * <p>
+ * CCL (Cursor Control Language) - 游标控制语言
+ * 用途: 用于控制游标操作
+ * 主要命令:
+ * DECLARE CURSOR: 定义游标
+ * OPEN: 打开游标
+ * FETCH: 获取游标数据
+ * CLOSE: 关闭游标
+ * </p>
+ *
+ * @author MC_Yang
+ * @version V1.0
+ **/
+package tt.smart.agency.sql.builder.sql.ccl;
---
+++ smart-agency-db/src/main/java/tt/smart/agency/sql/builder/sql/tcl/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * <p>
+ * TCL (Transaction Control Language) - 事务控制语言
+ * 用途: 用于管理数据库中的事务
+ * 主要命令:
+ * COMMIT: 提交事务
+ * ROLLBACK: 回滚事务
+ * SAVEPOINT: 设置保存点
+ * SET TRANSACTION: 设置事务属性
+ * </p>
+ *
+ * @author MC_Yang
+ * @version V1.0
+ **/
+package tt.smart.agency.sql.builder.sql.tcl;
